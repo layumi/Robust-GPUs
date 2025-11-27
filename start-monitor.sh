647e2f789a27c3b98f2c8510e65af3390cd5824e
Zhedong Zheng <zdzheng12@gmail.com>
2025-11-27 13:45:12 +0800

    Update start-monitor.sh

--- start-monitor.sh
+++ start-monitor.sh
@@ -1,1 +1,9 @@
-gunicorn -w 4 -b 0.0.0.0:5000 monitor-v7:app
+gunicorn -w 4 \
+    -k gevent \
+    --worker-connections 1000 \
+    -b 0.0.0.0:5000 \
+    --timeout 600 \
+    --graceful-timeout 30 \
+    --keep-alive 5 \
+    --log-level info \
+    monitor-v7:app
